30792212c90de130b9e9f2fa13584498191a63fe
RohitKuDwivedi <rdwivedi345@gmail.com>
2018-10-17 18:55:47 +0530

    branched seprate for mithila and rohit

---
+++ rohit/backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+const cors = require('cors');
+app.use(cors());
+app.use(bodyParser.json());
+//mongoose connection database
+const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true)
+mongoDB='mongodb://localhost/my_database';
+mongoose.connect(mongoDB, {useNewUrlParser: true});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database ' + mongoDB);
+});
+mongoose.connection.on('error', () => {
+    console.log('Connection error');
+});
+
+const port = 3000 
+
+
+const user = require('./routes/user')
+app.use('/registeration',user);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ rohit/backend/routes/user.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const Contacts = require('../models/contacts');
+const Functions = require('../utils/commonFun');
+const router = express.Router();
+
+router.post("/", (req, res) => {
+    Contacts.create(req.body, (err, user) => {
+        if (err) Functions.sendRes(res, false, "User can't be created");
+        else Functions.sendRes(res, true,
+            "User Created, UserID: " + user._id);
+        }
+       )
+});
+module.exports = router;
